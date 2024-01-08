a834e73bb8b96222abf5b94aeb670dbc4074ee6d
JOOWON KOH <67991485+joowon-dev@users.noreply.github.com>
2024-01-08 15:08:39 +0800

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st 
+
+pickle_in = open("req_RF.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+
+def predict_note_authentication(variance,skewness,curtosis,entropy):
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return prediction
+
+def main():
+    st.title("Bank Authenticator")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    variance = st.text_input("variance","Type Here")
+    skewness = st.text_input("skewness","Type Here")
+    curtosis = st.text_input("curtosis","Type Here")
+    entropy = st.text_input("entropy","Type Here")
+    result=""
+    if st.button("Predict"):
+        result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets Learn Python")
+        st.text("Built with Streamlit")
+
+if __name__ == '__main__':
+    main()
