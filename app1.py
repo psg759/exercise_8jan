77e960a2cb005912497627f51be52adfa93f5594
JOOWON KOH <67991485+joowon-dev@users.noreply.github.com>
2024-01-08 15:49:47 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,30 +1,36 @@
+\#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import numpy as np
 import pickle
 import pandas as pd
 import streamlit as st 
 
-pickle_in = open("reg_RF.pkl","rb")
-reg=pickle.load(pickle_in)
+pickle_in = open("RF.pkl","rb")
+rf=pickle.load(pickle_in)
 
 
-def predict_note_authentication(hour_s,pre_score):
-    prediction=reg.predict([[hour_s,pre_score]])
+def predict_std_perf(hour_s, pre_score):
+    prediction=rf.predict([[hour_s, pre_score]])
     print(prediction)
     return prediction
 
 def main():
-    st.title("Bank Authenticator")
+    st.title("Student Performance Prediction")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
-    variance = st.text_input("variance","Type Here")
-    skewness = st.text_input("skewness","Type Here")
+    hour_s = st.text_input("hour of study","Type Here")
+    pre_score = st.text_input("previous score", "Type Here")
     result=""
     if st.button("Predict"):
-        result=predict_note_authentication(hour_s,pre_score)
+        result=predict_std_perf(hour_s, pre_score)
     st.success('The output is {}'.format(result))
     if st.button("About"):
         st.text("Lets Learn Python")
